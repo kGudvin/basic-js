582d8c624fba983ae5a158a17d575a1cabbf3f74
Kirill_Gudeta <kirillgudeta@gmail.com>
2024-12-22 00:05:01 +0300

    feat: 2 done

--- src/common-character-count.js
+++ src/common-character-count.js
@@ -1,4 +1,4 @@
-const { NotImplementedError } = require('../extensions/index.js');
+const { NotImplementedError } = require('../extensions/index.js')
 
 /**
  * Given two strings, find the number of common characters between them.
@@ -11,11 +11,32 @@ const { NotImplementedError } = require('../extensions/index.js');
  * For s1 = "aabcc" and s2 = "adcaa", the output should be 3
  * Strings have 3 common characters - 2 "a"s and 1 "c".
  */
-function getCommonCharacterCount(/* s1, s2 */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function getCommonCharacterCount(s1, s2) {
+	function createHashTableFromString(str) {
+		const hashTable = {}
+
+		for (const char of str) {
+			if (hashTable[char]) {
+				hashTable[char]++
+			} else {
+				hashTable[char] = 1
+			}
+		}
+		return hashTable
+	}
+	const hashMapS1 = createHashTableFromString(s1)
+	const hashMapS2 = createHashTableFromString(s2)
+
+	const hashMapS1Keys = Object.keys(hashMapS1)
+	const hashMapS2Keys = Object.keys(hashMapS2)
+
+	const commonKeys = hashMapS1Keys.filter(key => hashMapS2Keys.includes(key))
+	const resultArray = commonKeys.map((el, index) => {
+		return hashMapS1[el] >= hashMapS2[el] ? hashMapS2[el] : hashMapS1[el]
+	})
+	return resultArray.reduce((acc, value) => acc + value, 0)
 }
 
 module.exports = {
-  getCommonCharacterCount
-};
+	getCommonCharacterCount,
+}
