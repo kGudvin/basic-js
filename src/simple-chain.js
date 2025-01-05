76cd0f88c094677d75162f6761ebd9e82ba0fd4e
Kirill_Gudeta <kirillgudeta@gmail.com>
2025-01-05 19:18:53 +0300

    feat: 60 pass

--- src/simple-chain.js
+++ src/simple-chain.js
@@ -1,32 +1,43 @@
-const { NotImplementedError } = require('../extensions/index.js');
+const { NotImplementedError } = require('../extensions/index.js')
 
 /**
  * Implement chainMaker object according to task description
- * 
+ *
  */
 const chainMaker = {
-  getLength() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
-  },
-  addLink(/* value */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
-  },
-  removeLink(/* position */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
-  },
-  reverseChain() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
-  },
-  finishChain() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
-  }
-};
+	chain: [],
+	getLength() {
+		return this.chain.length
+	},
+	addLink(value) {
+		this.chain.push(`( ${value !== undefined ? value : ''} )`)
+		return this
+	},
+	removeLink(position) {
+		if (
+			typeof position !== 'number' ||
+			position < 1 ||
+			position > this.chain.length ||
+			!Number.isInteger(position)
+		) {
+			this.chain = []
+			throw new Error("You can't remove incorrect link!")
+		} else {
+			this.chain.splice(position - 1, 1)
+			return this
+		}
+	},
+	reverseChain() {
+		this.chain.reverse()
+		return this
+	},
+	finishChain() {
+		const result = this.chain.join('~~')
+		this.chain = []
+		return result
+	},
+}
 
 module.exports = {
-  chainMaker
-};
+	chainMaker,
+}
